585138ca1256f4c05d9c0872b3d299c5d1a89d74
Lucas Pereira <lpereira@ext.getaroom.com>
2023-10-02 13:57:28 -0300

    Improve LogParser service and add spec tests to it

--- app/services/log_parser.rb
+++ app/services/log_parser.rb
@@ -1,63 +1,80 @@
-module LogParser
+class LogParser
   def self.generate_grouped_information(file_name="game_log.log.txt")
-    file_path = Rails.root.join('public', 'game_logs', file_name)
-
+    file_path = Rails.root.join("public", "game_logs", file_name)
     file_content = File.read(file_path)
-    matches = split_into_matches(file_content)
-    grouped_matches = group_info_by_match(matches)
+    matches_parser = MatchParser.new(file_content)
+    parsed_matches = matches_parser.parse
   end
 
-  private
-
-  def self.split_into_matches(text)
-    matches = text.split(/(?=#{Regexp.escape("InitGame")})/).filter { |potential_match| potential_match.include?("InitGame") }
-  end
-
-  def self.group_info_by_match(matches)
-    result = {}
-    matches.each_with_index do |match, i|
-      result["game_#{i+1}"] = parse_match_info(match)
+  class MatchParser
+    def initialize(file_content)
+      @file_content = file_content
     end
-    result
-  end
 
-  def self.parse_match_info(match)
-    return { "total_kills": 0, "players": [], "kills": {} } unless match.include?("killed")
+    def parse
+      matches = split_into_matches(@file_content)
+      group_info_by_match(matches)
+    end
 
-    hashed_match = {}
-    kills = match.split("\n").filter { |potential_kill| potential_kill.include?("Kill:") }
-    parse_kills(kills)
-  end
+    private
 
-  def self.parse_kills(kills)
-    result = {}
-    players = []
-    kills_count = {}
+    def split_into_matches(text)
+      matches = text.split(/(?=#{Regexp.escape("InitGame")})/).filter { |potential_match| potential_match.include?("InitGame") }
+    end
 
-    kills.each do |game_kill|
-      array = game_kill.split(' ')
-      reference_index = array.index("killed")
-      killer_player = array[reference_index - 1]
-      killed_player = array[reference_index + 1]
-      if killer_player == "<world>"
-        if kills_count[killed_player].present?
-          kills_count[killed_player]-+1
-        else
-          kills_count[killed_player] = -1
-        end
-      else
-        players << killer_player unless players.include?(killer_player)
-        if kills_count[killer_player].present?
-          kills_count[killer_player] += 1
-        else
-          kills_count[killer_player] = 1
-        end
+    def group_info_by_match(matches)
+      result = {}
+      matches.each_with_index do |match, i|
+        result["game_#{i+1}"] = parse_match_info(match)
       end
-      players << killed_player unless players.include?(killed_player)
+      result
     end
-    result["total_kills"] = kills.length
-    result["players"] = players
-    result["kills"] = kills_count
-    result
+
+    def parse_match_info(match)
+      hashed_match = {}
+      if match.include?("killed")
+        kills = match.split("\n").filter { |potential_kill| potential_kill.include?("Kill:") }
+        parse_kills(kills)
+      else
+        {
+          "total_kills" => 0,
+          "players" => [],
+          "kills" => {}
+        }
+      end
+    end
+
+    def parse_kills(kills)
+      result = {}
+      players = []
+      kills_count = {}
+
+      kills.each do |game_kill|
+        array = game_kill.split(" ")
+        reference_index = array.index("killed")
+        killer_player = array[reference_index - 1]
+        killed_player = array[reference_index + 1]
+        if killer_player == "<world>"
+          if kills_count[killed_player].present?
+            kills_count[killed_player]-=1
+          else
+            kills_count[killed_player] = -1
+          end
+        else
+          players << killer_player unless players.include?(killer_player)
+          if kills_count[killer_player].present?
+            kills_count[killer_player] += 1
+          else
+            kills_count[killer_player] = 1
+          end
+        end
+        players << killed_player unless players.include?(killed_player)
+      end
+
+      result["total_kills"] = kills.length
+      result["players"] = players
+      result["kills"] = kills_count
+      result
+    end
   end
 end
---
+++ spec/services/log_parser_spec.rb
@@ -0,0 +1,77 @@
+
+require "rails_helper"
+require "log_parser"
+
+RSpec.describe LogParser do
+  describe ".generate_grouped_information" do
+    subject {
+      LogParser.generate_grouped_information("sample_game_log.log.txt")
+    }
+
+    before do
+      file_content = "InitGame\nKill: player1 killed player2\nInitGame\nKill: player2 killed player3\n"
+      allow(File).to receive(:read).and_return(file_content)
+    end
+
+    it "should read and parse a game log file" do
+      expect_any_instance_of(LogParser::MatchParser).to receive(:parse)
+      subject
+    end
+  end
+
+  describe "MatchParser" do
+    before do
+      @match_parser = LogParser::MatchParser.new(file_content)
+    end
+
+    describe "#parse" do
+      let(:file_content) { "InitGame\nKill: player1 killed player2\nInitGame\nKill: player2 killed player3\n" }
+
+      it "should read and parse a game log file correctly" do
+        result = @match_parser.parse
+        expect(result).to be_a(Hash)
+        expect(result).to have_key("game_1")
+        expect(result).to have_key("game_2")
+        expect(result).to_not have_key("game_3")
+      end
+
+      context "when game logs includes more than one match" do
+        let(:file_content) { "InitGame\nKill: player1 killed player2\nKill: player3 killed player4\nInitGame\nKill: player1 killed player2\nKill: player3 killed player1\n" }
+
+        it "should read and parse a game log file correctly" do
+          result = @match_parser.parse
+          expect(result).to have_key("game_1")
+          expect(result).to have_key("game_2")
+          expect(result["game_1"]["total_kills"]).to eq(2)
+          expect(result["game_1"]["players"]).to eq(["player1", "player2", "player3", "player4"])
+          expect(result["game_1"]["kills"]).to eq({ "player1" => 1, "player3" => 1 })
+          expect(result["game_2"]["total_kills"]).to eq(2)
+          expect(result["game_2"]["players"]).to eq(["player1", "player2", "player3"])
+          expect(result["game_2"]["kills"]).to eq({ "player1" => 1, "player3" => 1 })
+        end
+      end
+
+      context "when logs include only matches with no kills" do
+        let(:file_content) { "InitGame\n" }
+
+        it "should return default values when no kills" do
+          result = @match_parser.parse
+          expect(result["game_1"]["total_kills"]).to eq(0)
+          expect(result["game_1"]["players"]).to be_empty
+          expect(result["game_1"]["kills"]).to be_empty
+        end
+      end
+
+      context "when there is a <world> killer in play" do
+        let(:file_content) { "InitGame\nKill: player1 killed player2\nKill: player2 killed player3\nKill: <world> killed player2\nKill: <world> killed player4" }
+
+        it "should parse kill information" do
+          result = @match_parser.parse
+          expect(result["game_1"]["kills"]["player1"]).to eq(1)
+          expect(result["game_1"]["kills"]["player2"]).to eq(0)
+          expect(result["game_1"]["kills"]["player4"]).to eq(-1)
+        end
+      end
+    end
+  end
+end
